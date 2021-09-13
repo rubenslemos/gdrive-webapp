0c9981d890d86b940c608093099c143a053b61d1
rubenslemos <rubenslemos@gmail.com>
2021-09-13 00:30:08 -0300

    add

---
+++ public/app.js
@@ -0,0 +1,23 @@
+import AppController from "./src/appController.js"
+import ConnectionManager from "./src/connectionManager.js"
+import DragAndDropManager from "./src/dragAndDropManager.js"
+import ViewManager from "./src/viewManager.js"
+DragAndDropManager
+//const API_URL = "https://localhost:3000"
+const API_URL = "https://exdrive-webapi.herokuapp.com/"
+
+const appController = new AppController({
+    viewManager: new ViewManager(),
+    dragAndDropManager: new DragAndDropManager(),
+    connectionManager: new ConnectionManager({
+        apiUrl: API_URL
+
+    })
+})
+try {
+    await appController.initialize()
+
+} catch (error) {
+
+    console.error("Error on initialize", error)
+}
